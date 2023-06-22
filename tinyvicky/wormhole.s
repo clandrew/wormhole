1742dc57de5da19e98c7087849495edc587e5040
ClAndrew <cmlandrews@gmail.com>
2023-06-21 18:27:20 -0700

    Cleanup

--- tinyvicky/wormhole.s
+++ tinyvicky/wormhole.s
@@ -603,63 +603,6 @@ MAIN
     and #$03
     sta $D103
 
-    ;;;;;;;;;;;;;;;
-
-    ; Set the line number to 0
-    stz line
-
-    ; Calculate the bank number for the bitmap
-    lda #(IMG_START >> 13)
-    sta bm_bank
-bank_loop: 
-    stz dst_pointer ; Set the pointer to start of the current bank
-    lda #$20
-    sta dst_pointer+1
-    ; Set the column to 0
-    stz column
-    stz column+1
-    ; Alter the LUT entries for $2000 -> $bfff
-
-    lda #$80 ; Turn on editing of MMU LUT #0, and use #0
-    sta MMU_MEM_CTRL
-    lda bm_bank
-    sta MMU_MEM_BANK_1 ; Set the bank we will map to $2000 - $3fff
-    stz MMU_MEM_CTRL ; Turn off editing of MMU LUT #0
-
-    ; Fill the line with the color..
-loop2_fillLine
-    lda line ; The line number is the color of the line
-
-    sta (dst_pointer)
-    inc_column: inc column ; Increment the column number
-    bne chk_col
-    inc column+1
-    chk_col: lda column ; Check to see if we have finished the row
-    cmp #<320
-    bne inc_point
-    lda column+1
-    cmp #>320
-    bne inc_point
-
-    LDA line ; If so, increment the line number
-    inc a
-    STA line
-    cmp #240 ; If line = 240, we’re done
-    beq Done_Init
-
-    stz column ; Set the column to 0
-    stz column+1
-    inc_point: inc dst_pointer ; Increment pointer
-    bne loop2_fillLine ; If < $4000, keep looping
-    inc dst_pointer+1
-    lda dst_pointer+1
-    cmp #$40
-    bne loop2_fillLine
-    inc bm_bank ; Move to the next bank
-    bra bank_loop ; And start filling it
-
-Done_Init
-
     JSR Init_IRQHandler
     
     LDA #$01
